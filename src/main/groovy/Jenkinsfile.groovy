20492548302d52b5ac2a89159b0e9e4202b2b5ff
espenkm <espenkm@gmail.com>
2017-01-17 22:08:51 +0100

    iFoo

--- src/main/groovy/Jenkinsfile.groovy
+++ src/main/groovy/Jenkinsfile.groovy
@@ -4,8 +4,9 @@ node {
   }
   stage('Download latest helm'){
     def helmObject = 'helm-v2.1.3-linux-amd64.tar'
-    def helmUrl = "https://storage.googleapis.com/kubernetes-charts-ci/helm/${helmObject}"
+    def helmUrl = "https://kubernetes-helm.storage.googleapis.com/${helmObject}"
     sh "wget -q ${helmUrl}"
+    sh "gunzip ${helmUrl}.gz"
     sh "tar xfv ${helmObject}"
   }
   stage('Install helm'){
